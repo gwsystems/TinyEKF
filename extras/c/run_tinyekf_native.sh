41b2e01e29ba060a33dbd8552963d378a3bde1ec
Sean McBride <spmcbride1201@gmail.com>
2021-04-05 15:05:09 -0400

    fix: Correct Makefile

--- extras/c/run_tinyekf_native.sh
+++ extras/c/run_tinyekf_native.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+if (($# < 1)); then
+  echo "Usage: $0 count"
+  exit 1
+fi
 
 ITERS=$1
 
-# before running this benchmark,
-# copy fibonacci to fibonacci_native.out
+testeach() {
+  tmp_cnt=${ITERS}
 
-testeach()
-{
-	tmp_cnt=${ITERS}
+  while [ "${tmp_cnt}" -gt 0 ]; do
+    ./gps_ekf_fn.out <ekf_raw.dat 1>out.dat
+    tmp_cnt=$((tmp_cnt - 1))
+  done
 
-	while [ ${tmp_cnt} -gt 0 ]; do
-		./gps_ekf_fn <ekf_raw.dat 1>out.dat
-		tmp_cnt=$((tmp_cnt - 1))
-	done
-
-	echo "Done!"
+  echo "Done!"
 }
 
-testeach 
+testeach
