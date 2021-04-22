76daf1da0546dd04896f20ae630cdeea0280c095
Sean McBride <spmcbride1201@gmail.com>
2021-04-22 15:48:26 +0000

    chore: Supress warnings

--- extras/c/wasm.mk
+++ extras/c/wasm.mk
@@ -16,7 +16,8 @@ LDFLAGS=-lm
 # NATIVE_CFLAGS=-g -I. -I../../src $(LDFLAGS)
 
 # Used with both NATIVE_CC and WASM_CC
-OPTFLAGS=-O3 -flto
+# -w = supress all warnings... We don't want to deviate from upstream source, so we won't fix anyways
+OPTFLAGS=-O3 -flto -w
 
 MEMC_64=64bit_nix.c
 
@@ -45,25 +46,25 @@ WASMISA=${SLEDGE_RT_DIR}/compiletime/instr.c
 
 SRC=../../src/tiny_ekf.c
 
-all: clean gps_ekf_fn.out gps_ekf_fn.so
+all: gps_ekf_fn.out gps_ekf_fn.so
 
 # Compile source into a WebAssembly Module. Unfortunately, this has to be recompiled targeting either aWsm or SLEdge
 gps_ekf_fn_out.wasm: $(SRC) gps_ekf_fn.c
-	${WASM_CC} ${WASMCFLAGS_OUT} ${OPTFLAGS} gps_ekf_fn.c ${SRC} ${DUMMY} -o gps_ekf_fn_out.wasm
+	@${WASM_CC} ${WASMCFLAGS_OUT} ${OPTFLAGS} gps_ekf_fn.c ${SRC} ${DUMMY} -o gps_ekf_fn_out.wasm
 
 gps_ekf_fn_so.wasm: $(SRC) gps_ekf_fn.c
-	${WASM_CC} ${WASMCFLAGS_SO} ${OPTFLAGS} gps_ekf_fn.c ${SRC} ${DUMMY} -o gps_ekf_fn_so.wasm
+	@${WASM_CC} ${WASMCFLAGS_SO} ${OPTFLAGS} gps_ekf_fn.c ${SRC} ${DUMMY} -o gps_ekf_fn_so.wasm
 
 
 # Compile the WebAssembly Module for direct execution as an ELF binary
 gps_ekf_fn.out: gps_ekf_fn_out.wasm
-	${AWSM_CC} gps_ekf_fn_out.wasm -o gps_ekf_fn_out.bc
-	${NATIVE_CC} ${NATIVE_CC_CFLAGS} ${OPTFLAGS} gps_ekf_fn_out.bc ${AWSM_RT_MEMC} ${AWSM_RT_LIBC} ${AWSM_RT_ENV} ${AWSM_RT_RT} -o gps_ekf_fn.out
+	@${AWSM_CC} gps_ekf_fn_out.wasm -o gps_ekf_fn_out.bc
+	@${NATIVE_CC} ${NATIVE_CC_CFLAGS} ${OPTFLAGS} gps_ekf_fn_out.bc ${AWSM_RT_MEMC} ${AWSM_RT_LIBC} ${AWSM_RT_ENV} ${AWSM_RT_RT} -o gps_ekf_fn.out
 
 # Compile the WebAssembly Module into a SLEdge serverless *.so module
 gps_ekf_fn.so: gps_ekf_fn_so.wasm
-	${AWSM_CC} --inline-constant-globals --runtime-globals gps_ekf_fn_so.wasm -o gps_ekf_fn_so.bc
-	${NATIVE_CC} --shared -fPIC ${OPTFLAGS} -I${SLEDGE_RT_INC} ${NATIVE_CC_CFLAGS} gps_ekf_fn_so.bc ${SLEDGE_MEMC} ${WASMISA} -o gps_ekf_fn.so
+	@${AWSM_CC} --inline-constant-globals --runtime-globals gps_ekf_fn_so.wasm -o gps_ekf_fn_so.bc
+	@${NATIVE_CC} --shared -fPIC ${OPTFLAGS} -I${SLEDGE_RT_INC} ${NATIVE_CC_CFLAGS} gps_ekf_fn_so.bc ${SLEDGE_MEMC} ${WASMISA} -o gps_ekf_fn.so
 
 # An early attempt at WASI support
 # gps_ekf_fn.wasi: $(SRC) gps_ekf_fn.c
@@ -78,5 +79,5 @@ gps_ekf_fn.so: gps_ekf_fn_so.wasm
 # 	${NATIVE_CC} --shared -fPIC ${OPTFLAGS} -D${USE_MEM} -I${ART_INC} ${EXTRA_CFLAGS} hello.bc ${AMEMC} ${WASMISA} -o hello.aso
 
 clean:
-	rm -f *.wasm *.bc *.out *.so
+	@rm -f *.wasm *.bc *.out *.so
 
